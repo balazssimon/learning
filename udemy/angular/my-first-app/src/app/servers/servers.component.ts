aa80b37b34bdafcffce0242c18ad77d2b3ac1299
Balazs Simon <balazssimon@outlook.com>
2017-11-15 15:53:21 +0100

    Bindings

--- udemy/angular/my-first-app/src/app/servers/servers.component.ts
+++ udemy/angular/my-first-app/src/app/servers/servers.component.ts
@@ -7,10 +7,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ServersComponent implements OnInit {
+  serverName = '';
+  allowNewServer = false;
+  serverCreated = false;
+  servers = ['Test server', 'Test server 2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => this.allowNewServer = true, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
   }
 
 }
